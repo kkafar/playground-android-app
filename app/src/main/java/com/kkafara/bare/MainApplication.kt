a31c50ad300ce74f07231d2077e72a45ed7b49e9
Kacper Kafara <kacperkafara@gmail.com>
2022-07-20 19:56:20 +0200

    chore: override some Application methods
    Added logging to them just to see when they're called.
    
    Squashed commit of the following:
    
    commit 4e1e513af2f439989301f190a7e5f75414a4fd93
    Author: Kacper Kafara <kacperkafara@gmail.com>
    Date:   Wed Jul 20 19:46:02 2022 +0200
    
        chore: override Application#onCreate method
    
    commit d27d445dc329d6508470a21b4bb7bf941adffbce
    Author: Kacper Kafara <kacperkafara@gmail.com>
    Date:   Wed Jul 20 19:45:38 2022 +0200
    
        fix: specify TAG as non-null type
    
    commit 56a5bfa46e4e945f69935336dafa8240ebf39188
    Author: Kacper Kafara <kacperkafara@gmail.com>
    Date:   Wed Jul 20 19:43:34 2022 +0200
    
        chore: override Application#onConfigurationChanged method
    
        Just adding some logging to see when its called

--- app/src/main/java/com/kkafara/bare/MainApplication.kt
+++ app/src/main/java/com/kkafara/bare/MainApplication.kt
@@ -1,8 +1,19 @@
 package com.kkafara.bare
 
 import android.app.Application
+import android.content.res.Configuration
+import android.util.Log
 
 class MainApplication : Application() {
+  val TAG: String = MainActivity::class.java.simpleName;
 
+  override fun onConfigurationChanged(newConfig: Configuration) {
+    Log.i(TAG, "onConfigurationChanged")
+    super.onConfigurationChanged(newConfig)
+  }
 
+  override fun onCreate() {
+    Log.i(TAG, "onCreate")
+    super.onCreate()
+  }
 }
